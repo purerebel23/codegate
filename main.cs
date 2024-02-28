cbd91dfb3b3c76c00915aa85e7a7e8a845608ff7
Robyn Romain <161603454+purerebel23@users.noreply.github.com>
2024-02-28 15:16:49 +0200

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,26 @@
+ foreach (var pm in brands)
+            {
+                var manufacturer = pm.Manufacturer;
+
+                if (!cachedModels.TryGetValue(manufacturer.Id, out BrandOverviewModel item))
+                {
+                    item = new BrandOverviewModel
+                    {
+                        Id = manufacturer.Id,
+                        Name = manufacturer.GetLocalized(x => x.Name),
+                        Description = manufacturer.GetLocalized(x => x.Description, true),
+                        SeName = manufacturer.GetActiveSlug()
+                    };
+ if (!ModelState.IsValid)
+            {
+                return false;
+            }
+
+            var paymentInfo = await paymentMethod.GetPaymentInfoAsync(form);
+            HttpContext.Session.TrySetObject("OrderPaymentInfo", paymentInfo);
+
+            var state = _checkoutStateAccessor.CheckoutState;
+            state.PaymentSummary = await paymentMethod.GetPaymentSummaryAsync();
+
+            return true;
+        }
